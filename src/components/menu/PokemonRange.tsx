8a2e5984bb9d99f47793df9887388b3072f147ee
Giovanni León <gleocor@contactel.es>
2026-01-12 13:06:45 +0000

    🐛 fix: corregir el manejo del tamaño de página en PokemonRange

--- src/components/menu/PokemonRange.tsx
+++ src/components/menu/PokemonRange.tsx
@@ -2,18 +2,23 @@ import { useRef } from 'react';
 import Constants from '../../Constants';
 import './PokemonRange.css';
 
-export default function PokemonRange({ pageSize: end, setPageSize: setEnd }: { pageSize: number, setPageSize: React.Dispatch<React.SetStateAction<number>> }) {
+export default function PokemonRange({ pageSize, setPageSize }: { pageSize: number, setPageSize: React.Dispatch<React.SetStateAction<number>> }) {
 
     const handleEndChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = Number(e.target.value);
+        let value = Number(e.target.value);
 
-        if (!isNaN(value) && value >= Constants.MIN_POKEMON_PER_PAGE) {
-            setEnd(value);
-        } else if (value > Constants.MAX_POKEMON_PER_PAGE) {
-            setEnd(Constants.MAX_POKEMON_PER_PAGE);
-        } else {
-            setEnd(Constants.MIN_POKEMON_PER_PAGE);
+        if (isNaN(value)) {
+            setPageSize(Constants.MIN_POKEMON_PER_PAGE);
+            return;
         }
+
+        if (value < Constants.MIN_POKEMON_PER_PAGE) {
+            value = Constants.MIN_POKEMON_PER_PAGE;
+        } else if (value > Constants.MAX_POKEMON_PER_PAGE) {
+            value = Constants.MAX_POKEMON_PER_PAGE;
+        }
+
+        setPageSize(value);
     };
 
     const inputRangeRef = useRef<HTMLInputElement>(null);
@@ -25,7 +30,7 @@ export default function PokemonRange({ pageSize: end, setPageSize: setEnd }: { p
     return (
         <div className="pokemon-range" onClick={focus}>
             <label htmlFor="input-range">Tamaño de página: </label>
-            <input ref={inputRangeRef} id='input-range' type="number" value={end} onChange={handleEndChange} min={Constants.MIN_POKEMON_PER_PAGE} max={Constants.MAX_POKEMON_PER_PAGE} />
+            <input ref={inputRangeRef} id='input-range' type="number" value={pageSize} onChange={handleEndChange} min={Constants.MIN_POKEMON_PER_PAGE} max={Constants.MAX_POKEMON_PER_PAGE} />
         </div>
     )
 }
