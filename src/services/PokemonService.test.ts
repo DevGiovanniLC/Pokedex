08878032541212aab62566882cd38daa9af7b2e4
Giovanni León <gleocor@contactel.es>
2026-01-12 11:44:00 +0000

    🧪 test(PokemonService): Hace test de los metodos de PokemonService

---
+++ src/services/PokemonService.test.ts
@@ -0,0 +1,35 @@
+jest.mock("../Constants", () => ({
+    __esModule: true,
+    default: {
+        API: "https://pokeapi.co/api/v2",
+        MAX_POKEMON_ID: 1025,
+    },
+}));
+
+import PokemonService from "./PokemonService";
+
+describe("PokemonService tests", () => {
+    it("fetches a Pokemon by ID", async () => {
+        const pokemon = await PokemonService.fetchPokemon(1);
+        expect(pokemon.name).toBe("bulbasaur");
+    });
+
+    it("fetches a Pokemon by ID 2", async () => {
+        const pokemon = await PokemonService.fetchPokemon(2);
+        expect(pokemon.name).toBe("ivysaur");
+    });
+
+    it("loads first page of Pokemons", async () => {
+        const pokemons = await PokemonService.loadPokemons(1, 5);
+        expect(pokemons.length).toBe(5);
+        expect(pokemons[0].name).toBe("bulbasaur");
+        expect(pokemons[3].name).toBe("charmander");
+    });
+
+    it("loads second page of Pokemons", async () => {
+        const pokemons = await PokemonService.loadPokemons(2, 6);
+        expect(pokemons.length).toBe(6);
+        expect(pokemons[0].name).toBe("squirtle");
+        expect(pokemons[2].name).toBe("blastoise");
+    });
+});
