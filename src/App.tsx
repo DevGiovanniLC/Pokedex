c963448b4ddddfd227a06452f8c117abd203bc37
Giovanni León <gleocor@contactel.es>
2026-01-12 11:11:43 +0000

    🐛 fix: implementar carga diferida para Pokedex y DetailedPokemon en App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from 'react-router'
-import Pokedex from './pages/Pokedex'
 import HomePage from './pages/HomePage'
-import DetailedPokemon from './pages/DetailedPokemon'
+import { lazy, Suspense } from 'react';
+const Pokedex = lazy(() => import('./pages/Pokedex'));
+const DetailedPokemon = lazy(() => import('./pages/DetailedPokemon'));
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/*" element={<><HomePage /></>} />
-            <Route path="/" element={<><HomePage /></>} />
-            <Route path="/pokedex" element={<><Pokedex /></>} />
-            <Route path="/pokedex/:pokemonId" element={<><DetailedPokemon /></>} />
-        </Routes>
+        <Suspense>
+            <Routes>
+                <Route path="/*" element={<><HomePage /></>} />
+                <Route path="/" element={<><HomePage /></>} />
+                <Route path="/pokedex" element={<><Pokedex /></>} />
+                <Route path="/pokedex/:pokemonId" element={<><DetailedPokemon /></>} />
+            </Routes>
+        </Suspense>
     )
 }
