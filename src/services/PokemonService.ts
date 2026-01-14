4dce360fc888ffa166eb17d74abcb8fe2863be45
Giovanni León <gleocor@contactel.es>
2026-01-14 09:53:27 +0000

    🐛 fix: simplificar la obtención de la lista de Pokémon y eliminar cachés innecesarios en PokemonService

--- src/services/PokemonService.ts
+++ src/services/PokemonService.ts
@@ -2,46 +2,34 @@ import Constants from "../Constants";
 import type { Pokemon, PokemonPreview } from "../models/Pokemon";
 
 export default class PokemonService {
-    private static readonly cachedPokemons: Map<number, Pokemon> = new Map();
-    private static readonly cachedImages: Set<string> = new Set();
     private static readonly PokemonPreviewCache: PokemonPreview[] = [];
 
-    static async findPokemonList(page: number, pageSize: number): Promise<Pokemon[]> {
-        this.clearCache();
-        const promises = await this.fetchPokemonList(page, pageSize);
-        return promises
-            .filter(result => result.status === 'fulfilled')
-            .map(result => (result as PromiseFulfilledResult<Pokemon>).value);
+    static async getPokemonListPreviewFilterBy(pokemonName: string): Promise<PokemonPreview[]> {
+        const pokemons = await this.getPokemonListPreview();
+        if (!pokemonName || pokemonName.trim() === '') return pokemons;
+        return pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(pokemonName.toLowerCase()));
     }
 
-    static async getPokemonListPreview(page: number, pageSize: number): Promise<PokemonPreview[]> {
-        const startId = (page - 1) * pageSize + 1;
-        const endId = page * pageSize;
-
+    private static async getPokemonListPreview(): Promise<PokemonPreview[]> {
         if (this.PokemonPreviewCache.length > 0) {
-            return this.PokemonPreviewCache.slice(startId - 1, endId);
+            return this.PokemonPreviewCache;
         }
-
-        return this.fetchPokemonListPreview(startId, endId);
+        return this.fetchPokemonListPreview();
     }
 
     static async findPokemon(id: number): Promise<Pokemon> {
-        if (this.cachedPokemons.has(id)) return this.cachedPokemons.get(id)!;
         const data = await this.fetchPokemon(id);
-        this.cachePokemon(id, data);
         return data;
     }
 
-    private static async fetchPokemonList(page: number, pageSize: number): Promise<PromiseSettledResult<Pokemon>[]> {
-        const startId = (page - 1) * pageSize + 1;
-        const endId = page * pageSize;
+    static async fetchPokemonListPreview(): Promise<PokemonPreview[]> {
+        const response = await fetch(
+            `${Constants.API}/pokemonlist`,
+        );
 
-        const promises = [];
-        for (let i = startId; i <= endId; i++) {
-            if (i >= Constants.MAX_POKEMON_ID) break;
-            promises.push(this.findPokemon(i));
-        }
-        return Promise.allSettled(promises);
+        const data: PokemonPreview[] = await response.json();
+        this.PokemonPreviewCache.push(...data);
+        return this.PokemonPreviewCache;
     }
 
     private static async fetchPokemon(id: number): Promise<Pokemon> {
@@ -52,30 +40,5 @@ export default class PokemonService {
         return data;
     }
 
-    private static cachePokemon(id: number, data: Pokemon): void {
-        this.cachedPokemons.set(id, data);
 
-        //Comprobación para el funcionamiento en entornos sin DOM #TESTS
-        if (typeof window !== 'undefined' && typeof Image !== 'undefined') {
-            const image = new Image()
-            this.cachedImages.add(
-                image.src = data.sprites.other?.["official-artwork"]?.front_default || ""
-            );
-        }
-    }
-
-    static async fetchPokemonListPreview(startId: number, endId: number): Promise<PokemonPreview[]> {
-        const response = await fetch(
-            `${Constants.API}/pokemonlist`,
-        );
-
-        const data: PokemonPreview[] = await response.json();
-        this.PokemonPreviewCache.push(...data);
-        return data.slice(startId - 1, endId);
-    }
-
-    private static clearCache(): void {
-        this.cachedPokemons.clear();
-        this.cachedImages.clear();
-    }
 }
