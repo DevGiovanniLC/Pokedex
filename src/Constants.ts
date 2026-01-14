2c08b2d4d1afa289e7a92d45ec98178c1e5de886
Giovanni León <gleocor@contactel.es>
2026-01-14 07:22:26 +0000

    🐛 fix: manejar el error en la obtención del ID máximo de Pokémon en Constants.ts

--- src/Constants.ts
+++ src/Constants.ts
@@ -8,7 +8,7 @@ const Constants = {
     MAX_POKEMON_PAGE: 50,
 
     API: 'http://localhost:3000',
-    MAX_POKEMON_ID: await fetch('http://localhost:3000/pokemon').then(res => res.json()).then(data => data.total),
+    MAX_POKEMON_ID: await fetch('http://localhost:3000/pokemon').then(res => res.json()).then(data => data.total).catch(() => 151),
 }
 
 export default Constants;
